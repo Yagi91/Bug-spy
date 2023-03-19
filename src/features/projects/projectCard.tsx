92f756e4eba3dd5438f1626af4029f3e848452f9
Yagi91 <bryantimah@gmail.com>
2023-03-19 01:22:34 +0100

    Make ProjectCard template

--- src/features/projects/projectCard.tsx
+++ src/features/projects/projectCard.tsx
@@ -5,16 +5,19 @@ interface Props {
     date: string;
     admin: string;
     progress: "Completed" | "Ongoing";
-    handleClick?: (name: string, event?: React.MouseEvent<HTMLDivElement>) => void;
+    handleClick: (event: React.MouseEvent<HTMLDivElement>, name: string) => void;
+    handleEdit?: (event: React.MouseEvent<HTMLDivElement>) => void;
+
 }
 
-export default function ProjectsCard({ name, date, admin, progress, handleClick }: Props) {
+export default function ProjectsCard({ name, date, admin, progress, handleClick, handleEdit }: Props): JSX.Element {
     return (
-        <div className="project-card">
+        <div className="project-card" onClick={(e) => handleClick(e, name)}>
             <p className="project-card-name">Name:{name}</p>
             <p>Admin: {admin}</p>
             <div>{date}</div>
             <p>Progress: {progress}</p>
+            <div onClick={(e) => { if (typeof handleEdit === 'function') return handleEdit(e) }} >Edit</div>
         </div>
     )
 }
