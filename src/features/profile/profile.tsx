f1e11951cb1764b5d7deca37b88b7e680348c83f
Yagi91 <bryantimah@gmail.com>
2023-07-31 02:51:55 +0100

    style profile screen

--- src/features/profile/profile.tsx
+++ src/features/profile/profile.tsx
@@ -1,73 +1,143 @@
 import React from "react";
 import { SimpleInput } from "../common/common";
 import Select from "react-select";
+import { getUser } from "./userSlice";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import userImage from "../../assets/images/3d-user.png";
 
 interface Props {
   userId: string;
 }
 
 export default function Profile({ userId }: Props): JSX.Element {
+  const user = useAppSelector((state) => state.user.user);
+  const dispatch = useAppDispatch();
+  React.useEffect(() => {
+    if (user.name) return;
+    dispatch(getUser(userId));
+  }, [dispatch, userId, user.name, user]);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  //get timezone
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
   return (
-    <div className="w-full border">
-      <div className="grid w-full grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 border border-red-500 lg:grid-cols-3">
-        <div className="border">
-          <div>
-            <div>Image</div>
-            <div>
-              <p>Name</p>
-              <p>role</p>
+    <div className="w-full text-gray-800">
+      <div className="grid w-full grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 lg:grid-cols-3">
+        <div className="rounded-[12px] bg-white px-3 py-2">
+          <div className="flex gap-4">
+            <img
+              src={userImage}
+              alt="user"
+              className="h-20 w-20 rounded-full"
+            />
+            <div className="flex flex-col justify-center text-left">
+              <p className="font-semibold capitalize">{user.name}</p>
+              <p className="capitalize">{user.role}</p>
             </div>
           </div>
         </div>
-        <div className="row-span-2 border lg:col-span-2">
-          <h2>General Information</h2>
-          <form>
+        <div className="row-span-2 rounded-[12px] bg-white px-3 py-2 lg:col-span-2">
+          <h2 className="text-left text-lg font-bold text-neutral-800">
+            General Information
+          </h2>
+          <form className="flex flex-col gap-5">
             <SimpleInput
               label="Name"
               name="name"
               type="text"
-              placeholder="Name"
+              placeholder={user.name}
+              extraClass="w-full placeholder:capitalize"
+              containerClass="[&>span]:mb-2 [&>span]:font-normal mt-4"
             />
             <SimpleInput
-              label="email"
+              label="Email"
               name="email"
-              type="text"
-              placeholder="email"
+              type="email"
+              placeholder={user.email}
+              extraClass="invalid:ring-red-300 w-full"
+              containerClass="[&>span]:mb-2 [&>span]:font-normal"
             />
             <Select
               options={[
-                { value: "admin", label: "Admin" },
-                { value: "developer", label: "Developer" },
-                { value: "tester", label: "Tester" },
+                { value: "Manger", label: "Manger" },
+                { value: "Developer", label: "Developer" },
+                { value: "Tester", label: "Tester" },
               ]}
+              placeholder={user.role}
+              isSearchable={false}
+              styles={{
+                control: (baseStyles, state) => ({
+                  ...baseStyles,
+                  borderRadius: "0.75rem",
+                  borderWidth: "1px",
+                  textAlign: "left",
+                }),
+                option: (baseStyles, state) => ({
+                  ...baseStyles,
+                  borderRadius: state.isSelected ? "0.75rem" : "0",
+                  textAlign: "left",
+                }),
+              }}
             />
-            <button type="submit">Update</button>
+            <button
+              type="submit"
+              className="w-fit rounded-xl bg-primary-600 px-4 py-2 font-bold text-white hover:bg-primary-700 focus:outline-none focus:ring-4 focus:ring-primary-200"
+            >
+              Update
+            </button>
           </form>
         </div>
-        <div className="border">
-          <h2>Language & Time</h2>
-          <form>
-            <label id="language">Select Language</label>
+        <div className="rounded-[12px] border bg-white px-3 py-2">
+          <h2 className="text-left text-lg font-bold text-neutral-800">
+            Language & Time
+          </h2>
+          <form className="flex flex-col">
+            <label id="language" className="mt-4 text-left">
+              Select Language
+            </label>
             <Select
-              options={[
-                { value: "en", label: "English" },
-                { value: "fr", label: "French" },
-                { value: "de", label: "German" },
-              ]}
+              options={[{ value: "en", label: "English" }]}
               name="language"
               placeholder="English"
-              isDisabled={true}
+              styles={{
+                control: (baseStyles, state) => ({
+                  ...baseStyles,
+                  borderRadius: "0.75rem",
+                  borderWidth: "1px",
+                  textAlign: "left",
+                }),
+                option: (baseStyles, state) => ({
+                  ...baseStyles,
+                  borderRadius: state.isSelected ? "0.75rem" : "0",
+                  textAlign: "left",
+                }),
+              }}
             />
-            <label id="timezone">Time zone</label>
+            <label id="timezone" className="mt-4 text-left">
+              Time zone
+            </label>
             <Select
-              options={[
-                { value: "GMT", label: "GMT" },
-                { value: "GMT+1", label: "GMT+1" },
-                { value: "GMT+2", label: "GMT+2" },
-              ]}
+              options={[{ value: "GMT", label: "GMT" }]}
               name="timezone"
-              placeholder="GMT+1"
+              placeholder={timezone}
               isDisabled={true}
+              styles={{
+                control: (baseStyles, state) => ({
+                  ...baseStyles,
+                  borderRadius: "0.75rem",
+                  borderWidth: "1px",
+                  textAlign: "left",
+                }),
+                option: (baseStyles, state) => ({
+                  ...baseStyles,
+                  borderRadius: state.isSelected ? "0.75rem" : "0",
+                  textAlign: "left",
+                }),
+              }}
             />
           </form>
         </div>
